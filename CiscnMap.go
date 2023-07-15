f6ba5c526a6903962d7bf31f6a465eb7fc5e1530
leijingkun <2092507229@qq.com>
2023-07-16 00:52:12 +0800

    nothing

--- CiscnMap.go
+++ CiscnMap.go
@@ -2,6 +2,19 @@ package main
 
 import "CiscnMap/utils"
 
+type Service struct {
+	port        int
+	Protocol    string
+	service_app string
+}
+
+type Host struct {
+	services   []Service
+	deviceinfo string
+	honeypot   string
+	timestamp  string
+}
+
 func main() {
 	utils.PortScan()
 }
